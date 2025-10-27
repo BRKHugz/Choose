b19f5d5fec171b9820d6db27516b6e600fd4ab6c
Lucas Vinicius <lucascarvalhoxs@gmail.com>
2025-10-27 00:45:17 -0300

    feat: adiciona classe Cadastro com atributos e métodos getters

---
+++ Cadastro.java
@@ -0,0 +1,29 @@
+package projeto_choose;
+
+public class Cadastro {
+
+    private String nome;
+    private String cpf;
+    private String regiao;
+    private int idade;
+    private String email;
+
+    public String getNome() {
+        return this.nome;
+    }
+    public String getCpf() {
+        return this.cpf;
+    }
+    public String getRegiao() {
+        return this.regiao;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+    public String getEmail() {
+        return this.email;
+    } 
+}
+
+
+
