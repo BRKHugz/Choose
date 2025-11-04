ba74de645e80e0d5855530acc6a5042932608660
Lucas Vinicius <lucasprogramador77@gmail.com>
2025-11-04 14:42:50 -0300

    feat(carros): implementa cadastro, busca, listagem e comparação de preços com a Tabela Fipe

--- src/main/java/com/choose/choose/controller/UsuarioController.java
+++ src/main/java/com/choose/choose/controller/UsuarioController.java
@@ -1,5 +1,34 @@
 package com.choose.choose.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.choose.choose.modelos.Usuario;
+import com.choose.choose.service.UsuarioService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    
+
+    @Autowired // usado para injetar o service no controller
+    private UsuarioService usuarioService;
+
+    @PostMapping("/cadastrar")
+    public String cadastrarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return "Usuário cadastrado!";
+    }
+
+    @GetMapping("/lista")
+    public List<Usuario> listarUsuario(){
+        return usuarioService.listarUsuario();
+    }
 }
--- src/main/java/com/choose/choose/service/UsuarioService.java
+++ src/main/java/com/choose/choose/service/UsuarioService.java
@@ -1,5 +1,23 @@
 package com.choose.choose.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.choose.choose.modelos.Usuario;
+
+@Service
 public class UsuarioService {
-    
+
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    public void salvarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+
+    public List<Usuario> listarUsuario(){
+        return usuarios;
+    }
+
 }
