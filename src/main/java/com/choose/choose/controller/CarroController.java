7b2eafa64e166a9fca2aa78d47b2d8ea8b43f7c1
Lucas Vinicius <lucasprogramador77@gmail.com>
2025-11-04 13:41:14 -0300

    feat: Cria os métodos de cadastrar carro, listar carros, comparar com a tabela fipe e buscar carros na lista de carros

--- src/main/java/com/choose/choose/controller/CarroController.java
+++ src/main/java/com/choose/choose/controller/CarroController.java
@@ -1,5 +1,54 @@
 package com.choose.choose.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.choose.choose.modelos.Carro;
+import com.choose.choose.service.CarroService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/carros")
 public class CarroController {
 
+    @Autowired
+    private CarroService serviceCarro; // Composição com atributo de uma outra classe
+
+    @PostMapping("/cadastrarCarro")
+    public String cadastrarCarro(@RequestBody Carro carro){
+        serviceCarro.salvarCarro(carro);
+        return "Carro cadastrado com sucesso!";
+    }
+    
+    @GetMapping("/listarCarros")
+    public List<Carro> listarCarros(){
+        return serviceCarro.listarCarros();
+    }
+
+    @GetMapping("/{modelo}")
+    public Carro buscarCarro(@PathVariable String modelo){
+        return serviceCarro.buscarModelo(modelo);
+    }
+
+    @GetMapping("/comparar/{modelo}")
+    public String comparaPreco(@PathVariable String modelo){
+        Carro carro = serviceCarro.buscarModelo(modelo);
+        Double precoFipe = serviceCarro.retornaValorFipe(modelo);
+
+        if(carro == null) return "Carro não encontrado";
+        
+        if(precoFipe == null) return "Modelo não encontrado na tabela de preços fipe";
+        
+
+        double diferenca = carro.getPreco() - precoFipe;
+
+        return "Preço do usuário é: "+carro.getPreco()+" | Preço FIPE: "+precoFipe+" | diferença de preço: "+diferenca;
+    }
 }
--- src/main/java/com/choose/choose/service/CarroService.java
+++ src/main/java/com/choose/choose/service/CarroService.java
@@ -1,5 +1,47 @@
 package com.choose.choose.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.choose.choose.modelos.Carro;
+
+@Service
 public class CarroService {
     
+    private final List<Carro> carros = new ArrayList<>(); // Lista de carros
+    private final Map<String, Double> tabelaFipe = new HashMap<>(); //Simulador de tabela fipe:
+
+    public CarroService(){
+        tabelaFipe.put("Gol", 50000.0);
+        tabelaFipe.put("Onix", 70000.0);
+        tabelaFipe.put("Civic", 120000.0);
+        tabelaFipe.put("Corolla", 130000.0);
+    }
+
+    public void salvarCarro(Carro carro){
+        carros.add(carro);
+    }
+
+    public List<Carro> listarCarros(){
+        return carros;
+    }
+
+    public Carro buscarModelo(String modelo){
+        for(Carro altomovel : carros){
+            if(altomovel.getModelo().equalsIgnoreCase(modelo)){
+                return altomovel;
+            }
+        }
+        return null;
+    }
+    
+    public double retornaValorFipe(String modelo){
+        return tabelaFipe.get(modelo);
+    }
+
+
 }
