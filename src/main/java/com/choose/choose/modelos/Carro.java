2b8cf698360dda7007616d668bdbe220d434249c
Lucas Vinicius <lucasprogramador77@gmail.com>
2025-12-01 15:58:03 -0300

    feat: adiciona mais atributos no modelo carro

--- src/main/java/com/choose/choose/modelos/Carro.java
+++ src/main/java/com/choose/choose/modelos/Carro.java
@@ -6,14 +6,28 @@ import lombok.Data; // serve como um biblioteca para gerar getters e setters, po
 public class Carro {
     private String modelo;
     private String marca;
+    private String versao;
+    private String cor;
+    private int quilometragem;
     private int ano;
     private double preco;
+    private String cambio;
+    private int quantidadeDePortas;
+    private String potenciaMotor;
 
     // construtor
-    public Carro(String modelo, String marca, int ano, double preco){
+    public Carro(
+        String modelo, String marca, String versao, int ano, double preco, String cambio, 
+        int quantidadeDePortas, String potenciaMotor, String cor, int quilometragem
+    ){   
+        this.cor = cor;
+        this.quilometragem = quilometragem;
         this.modelo = modelo;
         this.marca = marca;
+        this.versao = versao;
         this.ano = ano;
         this.preco = preco;
+        this.cambio = cambio;
+        this.quantidadeDePortas = quantidadeDePortas;
     }
 }
